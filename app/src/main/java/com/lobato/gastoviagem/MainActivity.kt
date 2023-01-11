2c2b0e721c74fa835342c0e815e4a96bfd5d3f78
Thyago Lobato <thyagollobato@gmail.com>
2023-01-11 17:06:25 -0300

    config: ViewBinding

--- app/src/main/java/com/lobato/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/lobato/gastoviagem/MainActivity.kt
@@ -2,10 +2,19 @@ package com.lobato.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lobato.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    private fun calculate() {
+    }
+
 }
