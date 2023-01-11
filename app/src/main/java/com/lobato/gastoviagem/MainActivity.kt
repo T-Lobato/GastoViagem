2fca83a801dc777132bfe5dabb1fc6acedb8d61c
Thyago Lobato <thyagollobato@gmail.com>
2023-01-11 18:20:54 -0300

    Finam do projeto

--- app/src/main/java/com/lobato/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/lobato/gastoviagem/MainActivity.kt
@@ -2,9 +2,11 @@ package com.lobato.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.lobato.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,9 +14,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != "")
+    }
+
 
     private fun calculate() {
-    }
 
+        var autonomy: Float = 0.0f
+
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            if (binding.editAutonomy.text.toString().toFloat() != 0f) {
+                autonomy = binding.editAutonomy.text.toString().toFloat()
+                val totalValue = (distance * price) / autonomy
+                binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+            } else {
+                Toast.makeText(this, R.string.validation_fill_different_zero, Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        } else Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+
+    }
 }
